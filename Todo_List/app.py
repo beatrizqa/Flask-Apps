4d75d8f49fcbcdc3c21e1c679d2439e667e66724
beatrizqa <beatriz.manzano@qa.com>
2021-05-11 17:01:33 +0100

    BM: create Flask directory with db configurations

--- Todo_List/app.py
+++ Todo_List/app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)  # create Flask object
 
 # Set the connection string to connect to the database
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.105.152.18/myflaskdb"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)  # create SQLALchemy object
 
 
 class Todos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(100), nullable=False)
-    complete = db.Column(db.Boolean, nullable=True)
+    complete = db.Column(db.Boolean, default=False)
 
 
 @app.route('/')
@@ -20,13 +20,13 @@ def index():
     return "This is a TODO list"
 
 
-@app.route('/todos')
-def showtodos():
-    all_todos = Todos.query.all()
-    todo_sum = ""
-    for todo in all_todos:
-        todo_sum = todo_sum + "<br>" + todo.task + " " + str(todo.complete)
-    return todo_sum
+# @app.route('/todos')
+# def showtodos():
+#     all_todos = Todos.query.all()
+#     todo_sum = ""
+#     for todo in all_todos:
+#         todo_sum = todo_sum + "<br>" + todo.task + " " + str(todo.complete)
+#     return todo_sum
 
 
 if __name__ == '__main__':
